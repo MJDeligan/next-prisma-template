d495c9c7a8ebb27f93c8c257c0d11996fd1ac1a8
MJDeligan <marco.razor@hotmail.com>
2023-02-16 21:00:30 +0100

    inject logger into auth wrappers

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,6 +5,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "./prisma";
 import GoogleProvider from "next-auth/providers/google";
 import { Role } from "@prisma/client";
+import { withLogger } from "./logger";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
@@ -51,37 +52,41 @@ export const getCurrentUser = async () => {
 export const withAuthenticationRequired = (
   handler: (req: NextApiRequest, res: NextApiResponse) => unknown
 ) => {
-  return async (req: NextAPIRequestWithLogger, res: NextApiResponse) => {
-    const session = await getServerSession(req, res, authOptions);
-    if (!session) {
-      req.log.warn("Unauthenticated tried accessing secured api route");
-      res.status(401).json({ error: "Unauthenticated" });
-      return;
+  return withLogger(
+    async (req: NextAPIRequestWithLogger, res: NextApiResponse) => {
+      const session = await getServerSession(req, res, authOptions);
+      if (!session) {
+        req.log.warn("Unauthenticated tried accessing secured api route");
+        res.status(401).json({ error: "Unauthenticated" });
+        return;
+      }
+      req.log.info("User authenticated");
+      return await handler(req, res);
     }
-    req.log.info("User authenticated");
-    return await handler(req, res);
-  };
+  );
 };
 
 export const withRequiredRole = (
   requiredRole: Role,
   handler: (req: NextApiRequest, res: NextApiResponse) => unknown
 ) => {
-  return async (req: NextAPIRequestWithLogger, res: NextApiResponse) => {
-    const session = await getServerSession(req, res, authOptions);
-    req.log.info(session);
-    if (!session) {
-      req.log.warn("Unauthenticated tried accessing secured api route");
-      res.status(401).json({ error: "Unauthenticated" });
-      return;
+  return withLogger(
+    async (req: NextAPIRequestWithLogger, res: NextApiResponse) => {
+      const session = await getServerSession(req, res, authOptions);
+      req.log.info(session);
+      if (!session) {
+        req.log.warn("Unauthenticated tried accessing secured api route");
+        res.status(401).json({ error: "Unauthenticated" });
+        return;
+      }
+      req.log.info("User authenticated");
+      const { role } = session.user;
+      if (!role || role !== requiredRole) {
+        req.log.warn("User tried to access route with incorrect role");
+        res.status(403).send({ error: "Forbidden" });
+        return;
+      }
+      return await handler(req, res);
     }
-    req.log.info("User authenticated");
-    const { role } = session.user;
-    if (!role || role !== requiredRole) {
-      req.log.warn("User tried to access route with incorrect role");
-      res.status(403).send({ error: "Forbidden" });
-      return;
-    }
-    return await handler(req, res);
-  };
+  );
 };
