0da407d0162d71602d465ae4ade268b1ac0105e4
MJDeligan <marco.razor@hotmail.com>
2023-02-17 15:41:47 +0100

    update logger wrapper type definition

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,10 +1,15 @@
 import { NextAPIRequestWithLogger } from "@/types";
-import { NextApiResponse } from "next";
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 import pino from "pino";
+
+type NextApiHandlerWithLogger = (
+  req: NextAPIRequestWithLogger,
+  res: NextApiResponse
+) => unknown;
 
 const productionLogLevel = process.env.PRODUCTION_LOG_LEVEL || "error";
 const logLevel =
-  process.env.NODE_ENV === "production" ? productionLogLevel : "info";
+  process.env.NODE_ENV === "production" ? productionLogLevel : "trace";
 
 const transport = pino.transport({
   targets: [
@@ -20,15 +25,16 @@ const transport = pino.transport({
 const serverLogger = pino(transport);
 
 export const withLogger = (
-  handler: (req: NextAPIRequestWithLogger, res: NextApiResponse) => unknown
-) => {
-  return async (req: NextAPIRequestWithLogger, res: NextApiResponse) => {
-    req.log = serverLogger.child({
+  handler: NextApiHandlerWithLogger
+): NextApiHandler => {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    const newReq = req as NextAPIRequestWithLogger;
+    newReq.log = serverLogger.child({
       requestId: req.headers["x-request-id"],
       url: req.url,
       method: req.method,
     });
 
-    return await handler(req, res);
+    return await handler(newReq, res);
   };
 };
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { withLogger } from "@/lib/logger";
 import { NextAPIRequestWithLogger } from "@/types";
 import type { NextApiResponse } from "next";
 
@@ -6,10 +7,10 @@ type Data = {
   name: string;
 };
 
-export default function handler(
+export default withLogger(function handler(
   req: NextAPIRequestWithLogger,
   res: NextApiResponse<Data>
 ) {
   req.log.info("Test");
   res.status(200).json({ name: "John Doe" });
-}
+});
